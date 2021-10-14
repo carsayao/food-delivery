3e56ff5cef4e5e257691912eae07d4ff864efdba
Carlos Miguel Sayao <carsayao@pdx.edu>
2021-10-13 18:18:11 -0700

    better balancing tree

--- src/com/company/Tree/BalancedTree.java
+++ src/com/company/Tree/BalancedTree.java
@@ -30,20 +30,21 @@ public class BalancedTree extends BinaryTree {
         c.setHeight(max(c.getLeftHeight(),
                 c.getRightHeight()) +1);
         this.balance = c.getBalance();
+        System.out.println("Balance: " + this.balance);
         // Left left, then return rotated subtree
-        if (this.balance > 1 && num < c.getLeft().getInt())
+        if (this.balance > 1 && num <= c.getLeft().getInt())
             return c.rotateRight();
         // Right right
-        if (this.balance <  -1 && num > c.getRight().getInt())
+        if (this.balance <  -1 && num >= c.getRight().getInt())
             return c.rotateLeft();
         // Left right, rotate left with left node as pivot,
         // then rotate right with current as pivot
-        if (this.balance > 1 && num > c.getLeft().getInt()) {
+        if (this.balance > 1 && num >= c.getLeft().getInt()) {
             c.setLeft(c.getLeft().rotateLeft());
             return c.rotateRight();
         }
         // Right left
-        if (this.balance < -1 && num < c.getRight().getInt()) {
+        if (this.balance < -1 && num <= c.getRight().getInt()) {
             c.setRight(c.getRight().rotateRight());
             return c.rotateLeft();
         }
